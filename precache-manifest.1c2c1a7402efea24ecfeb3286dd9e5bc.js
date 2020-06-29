a817604eaf7ab37c4f96cc99a42c72c2db917334
lavish kumar jangir <lavishkumar12@gmail.com>
2020-06-30 02:41:50 +0530

    Updates

---
+++ precache-manifest.1c2c1a7402efea24ecfeb3286dd9e5bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1998a69ce3ca1e5158a47e060a2962d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "747ec1e2bb815254c258",
+    "url": "/static/css/main.d6039dbe.chunk.css"
+  },
+  {
+    "revision": "383d04ff3b55c00ac78f",
+    "url": "/static/js/2.d00ef22f.chunk.js"
+  },
+  {
+    "revision": "041bfff3748217d0ac9f224633d66722",
+    "url": "/static/js/2.d00ef22f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747ec1e2bb815254c258",
+    "url": "/static/js/main.c7dd1b68.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
